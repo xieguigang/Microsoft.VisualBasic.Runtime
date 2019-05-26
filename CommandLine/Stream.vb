a42cb1f4f9f6212ff2ea3579ef5c70dafc809f4d
xieguigang <xie.guigang@gcmodeller.org>
2019-05-26 13:09:51 +0800

    get file type

--- CommandLine/Stream.vb
+++ CommandLine/Stream.vb
@@ -3,6 +3,12 @@ Imports System.IO.MemoryMappedFiles
 
 Namespace CommandLine
 
+    Public Enum FileTypes
+        DiskFile
+        PipelineFile
+        MemoryFile
+    End Enum
+
     ''' <summary>
     ''' + file path: C://path/to/file
     ''' + standard input: std_in://
@@ -10,6 +16,16 @@ Namespace CommandLine
     ''' + memory mapping file: memory://file/uri
     ''' </summary>
     Public Module StreamExtensions
+
+        Public Function FileType(reference As String) As FileTypes
+            If reference.TextEquals("std_in://") OrElse reference.TextEquals("std_out://") Then
+                Return FileTypes.PipelineFile
+            ElseIf reference.ToLower.StartsWith("memory://") Then
+                Return FileTypes.MemoryFile
+            Else
+                Return FileTypes.DiskFile
+            End If
+        End Function
 
         Public Function OpenForRead(reference As String) As Stream
             If reference.TextEquals("std_in://") Then
