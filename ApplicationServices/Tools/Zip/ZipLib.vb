7f29678088a9c115de2a6a1ec00ae39ebf73f2c2
xieguigang <xie.guigang@gcmodeller.org>
2019-06-29 00:33:24 +0800

    split source files

--- ApplicationServices/Tools/Zip/ZipLib.vb
+++ ApplicationServices/Tools/Zip/ZipLib.vb
@@ -69,7 +69,7 @@ Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Language.UnixBash
 Imports Microsoft.VisualBasic.Scripting.MetaData
 
-Namespace ApplicationServices
+Namespace ApplicationServices.Zip
 
 #If NET_40 = 0 Then
 
@@ -85,28 +85,6 @@ Namespace ApplicationServices
          Publisher:="Tim Corey",
          Url:="http://www.codeproject.com/Articles/381661/Creating-Zip-Files-Easily-in-NET")>
     Public Module ZipLib
-
-        ''' <summary>
-        ''' Used to specify what our overwrite policy
-        ''' is for files we are extracting.
-        ''' </summary>
-        Public Enum Overwrite
-            Always
-            IfNewer
-            Never
-        End Enum
-
-        ''' <summary>
-        ''' Used to identify what we will do if we are
-        ''' trying to create a zip file and it already
-        ''' exists.
-        ''' </summary>
-        Public Enum ArchiveAction
-            Merge
-            Replace
-            [Error]
-            Ignore
-        End Enum
 
         ''' <summary>
         ''' Unzips the specified file to the given folder in a safe
