2def46891d5bd88974b7db21c1da6a4d3b6648d2
xieguigang <xie.guigang@gcmodeller.org>
2019-07-17 21:37:36 +0800

    code style improvements

--- Resources/scripts/bashRunner.sh
+++ Resources/scripts/bashRunner.sh
@@ -1,21 +1,32 @@
 ﻿#!/bin/bash
 
+# target script may be a symlink
+# so the vb application may not exists in the 
+# same directory with current script's symlink
 SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+
+# resolve $SOURCE until the file is no longer a symlink
+while [ -h "$SOURCE" ]; do 
   TARGET="$(readlink "$SOURCE")"
+
   if [[ $TARGET == /* ]]; then
     echo "SOURCE '$SOURCE' is an absolute symlink to '$TARGET'"
     SOURCE="$TARGET"
   else
     DIR="$( dirname "$SOURCE" )"
     echo "SOURCE '$SOURCE' is a relative symlink to '$TARGET' (relative to '$DIR')"
-    SOURCE="$DIR/$TARGET" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    # if $SOURCE was a relative symlink, we need to resolve it 
+    # relative to the path where the symlink file was located
+    SOURCE="$DIR/$TARGET" 
   fi
 done
+
 echo "SOURCE is '$SOURCE'"
 RDIR="$( dirname "$SOURCE" )"
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
 if [ "$DIR" != "$RDIR" ]; then
   echo "DIR '$RDIR' resolves to '$DIR'"
 fi
+
 echo "DIR is '$DIR'"
