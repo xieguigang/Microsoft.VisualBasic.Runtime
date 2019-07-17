6cd0a4efc7f6649ce028d2ed62e68b2ae93680a8
xieguigang <xie.guigang@gcmodeller.org>
2019-07-17 21:42:01 +0800

    removes debug echo

--- Resources/scripts/bashRunner.sh
+++ Resources/scripts/bashRunner.sh
@@ -20,18 +20,20 @@ while [ -h "$SOURCE" ]; do
   TARGET="$(readlink "$SOURCE")"
 
   if [[ $TARGET == /* ]]; then
-    echo "SOURCE '$SOURCE' is an absolute symlink to '$TARGET'"
+    # echo "SOURCE '$SOURCE' is an absolute symlink to '$TARGET'"
     SOURCE="$TARGET"
   else
     DIR="$( dirname "$SOURCE" )"
-    echo "SOURCE '$SOURCE' is a relative symlink to '$TARGET' (relative to '$DIR')"
+    
+    # echo "SOURCE '$SOURCE' is a relative symlink to '$TARGET' (relative to '$DIR')"
+    
     # if $SOURCE was a relative symlink, we need to resolve it 
     # relative to the path where the symlink file was located
     SOURCE="$DIR/$TARGET" 
   fi
 done
 
-echo "SOURCE is '$SOURCE'"
+# echo "SOURCE is '$SOURCE'"
 
 # This last one will work with any combination of 
 # aliases, source, bash -c, symlinks, etc.
@@ -44,8 +46,8 @@ echo "SOURCE is '$SOURCE'"
 RDIR="$( dirname "$SOURCE" )"
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
-if [ "$DIR" != "$RDIR" ]; then
-  echo "DIR '$RDIR' resolves to '$DIR'"
-fi
+# if [ "$DIR" != "$RDIR" ]; then
+#  echo "DIR '$RDIR' resolves to '$DIR'"
+# fi
 
-echo "DIR is '$DIR'"
+# echo "DIR is '$DIR'"
