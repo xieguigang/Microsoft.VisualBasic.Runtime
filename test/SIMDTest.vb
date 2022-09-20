cce5ebc9a888c647ded1bc92e68aa7803c11c667
xieguigang <xie.guigang@gcmodeller.org>
2022-09-20 23:39:30 +0800

    SIMD is not working as expected

--- test/SIMDTest.vb
+++ test/SIMDTest.vb
@@ -1,13 +1,32 @@
 ﻿Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Math
+Imports randf = Microsoft.VisualBasic.Math.RandomExtensions
 
 Module SIMDTest
 
     Sub Main()
-        Dim a As Double() = 100000.Sequence.Select(Function(i) CDbl(i)).ToArray
-        Dim b As Double() = a
+        Dim nsize = 10000000
+        Dim a As Double() = nsize.Sequence.Select(Function(i) randf.NextDouble).ToArray
+        Dim b As Double() = nsize.Sequence.Select(Function(i) randf.NextDouble).ToArray
 
-        Dim c = SIMD.Add(a, b)
+        For i As Integer = 0 To 10
+            SIMD.enable = True
+
+            Dim t1 = App.NanoTime
+            Dim c = SIMD.Add(a, b)
+            Dim t2 = App.NanoTime
+
+            SIMD.enable = False
+
+            Dim t3 = App.NanoTime
+            Dim d = SIMD.Add(a, b)
+            Dim t4 = App.NanoTime
+
+            Call Console.WriteLine(c.SequenceEqual(d))
+            Call Console.WriteLine(t2 - t1)
+            Call Console.WriteLine(t4 - t3)
+            Call Console.WriteLine((t4 - t3) / (t2 - t1))
+        Next
 
         Pause()
     End Sub
