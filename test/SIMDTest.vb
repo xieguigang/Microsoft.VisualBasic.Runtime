a99cb9c3707032caede826b0b477364d41236fb7
xieguigang <xie.guigang@gcmodeller.org>
2022-09-20 22:42:54 +0800

    config for simd add test

---
+++ test/SIMDTest.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math
+
+Module SIMDTest
+
+    Sub Main()
+        Dim a As Double() = 100000.Sequence.Select(Function(i) CDbl(i)).ToArray
+        Dim b As Double() = a
+
+        Dim c = SIMD.Add(a, b)
+
+        Pause()
+    End Sub
+End Module
