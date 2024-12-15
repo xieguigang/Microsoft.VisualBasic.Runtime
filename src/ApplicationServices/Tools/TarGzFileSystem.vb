40c5addddec81aa8716ca8cc001ef520c1e868e7
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-15 23:27:24 +0800

    add supports for targz file

--- src/ApplicationServices/Tools/TarGzFileSystem.vb
+++ src/ApplicationServices/Tools/TarGzFileSystem.vb
@@ -3,6 +3,7 @@ Imports System.IO
 Imports System.IO.Compression
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
 
 Namespace ApplicationServices
 
@@ -12,7 +13,7 @@ Namespace ApplicationServices
 
         ReadOnly gz As GZipStream
         ReadOnly tar As TarReader
-        ReadOnly tree As FileSystemTree
+        ReadOnly tree As New FileSystemTree
 
         Public ReadOnly Property [readonly] As Boolean Implements IFileSystemEnvironment.readonly
             Get
@@ -28,17 +29,14 @@ Namespace ApplicationServices
 
             ' load files
             Dim entry As New Value(Of TarEntry)
-            Dim filenames As New List(Of String)
 
             Do While (entry = tar.GetNextEntry) IsNot Nothing
                 If CheckVirtualEntry(entry) Then
                     Continue Do
                 End If
 
-                Call filenames.Add("/" & CType(entry, TarEntry).Name)
+                tree.AddFile("/" & CType(entry, TarEntry).Name).data = CType(entry, TarEntry)
             Loop
-
-            tree = FileSystemTree.BuildTree(filenames)
         End Sub
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
@@ -60,7 +58,14 @@ Namespace ApplicationServices
                                  Optional mode As FileMode = FileMode.OpenOrCreate,
                                  Optional access As FileAccess = FileAccess.Read) As Stream Implements IFileSystemEnvironment.OpenFile
 
-            Dim entry As TarEntry = tar.
+            Dim entry As FileSystemTree = tree.GetFile(path)
+
+            If entry Is Nothing Then
+                Throw New MissingPrimaryKeyException(path)
+            End If
+
+            Dim tar As TarEntry = entry.data
+            Return tar.DataStream
         End Function
 
         Public Function DeleteFile(path As String) As Boolean Implements IFileSystemEnvironment.DeleteFile
@@ -68,19 +73,46 @@ Namespace ApplicationServices
         End Function
 
         Public Function FileExists(path As String, Optional ZERO_Nonexists As Boolean = False) As Boolean Implements IFileSystemEnvironment.FileExists
-            Throw New NotImplementedException()
+
+            Dim entry As FileSystemTree = tree.GetFile(path)
+
+            If entry Is Nothing Then
+                Return False
+            ElseIf ZERO_Nonexists Then
+                Return DirectCast(entry.data, TarEntry).Length > 0
+            Else
+                Return True
+            End If
         End Function
 
         Public Function FileSize(path As String) As Long Implements IFileSystemEnvironment.FileSize
-            Throw New NotImplementedException()
+            Dim entry As FileSystemTree = tree.GetFile(path)
+
+            If entry Is Nothing Then
+                Return -1
+            Else
+                Return DirectCast(entry.data, TarEntry).Length
+            End If
         End Function
 
         Public Function FileModifyTime(path As String) As Date Implements IFileSystemEnvironment.FileModifyTime
-            Throw New NotImplementedException()
+            Dim entry As FileSystemTree = tree.GetFile(path)
+
+            If entry Is Nothing Then
+                Return Nothing
+            Else
+                Return DirectCast(entry.data, TarEntry).ModificationTime.LocalDateTime
+            End If
         End Function
 
         Public Function GetFullPath(filename As String) As String Implements IFileSystemEnvironment.GetFullPath
-            Throw New NotImplementedException()
+            Dim entry As FileSystemTree = tree.GetFile(filename)
+
+            If entry Is Nothing Then
+                Return Nothing
+            End If
+
+            Return entry.ToString
         End Function
 
         Public Function WriteText(text As String, path As String) As Boolean Implements IFileSystemEnvironment.WriteText
@@ -88,11 +120,15 @@ Namespace ApplicationServices
         End Function
 
         Public Function ReadAllText(path As String) As String Implements IFileSystemEnvironment.ReadAllText
-            Throw New NotImplementedException()
+            Dim s As Stream = OpenFile(path, FileMode.Open, FileAccess.Read)
+            Dim str As New StreamReader(s)
+
+            Return str.ReadToEnd
         End Function
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Function GetFiles() As IEnumerable(Of String) Implements IFileSystemEnvironment.GetFiles
-            Throw New NotImplementedException()
+            Return tree.AsEnumerable
         End Function
     End Class
 #End If
