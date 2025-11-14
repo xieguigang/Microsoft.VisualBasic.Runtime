6d84d887b1d3c906c4608a4bbabdfc98e0b5f4c4
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-15 02:33:13 +0800

    add xml models for numeric vectors

---
+++ src/Text/Xml/Models/Numeric.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Xml.Serialization
+
+Namespace Text.Xml.Models
+
+    Public Class ints
+        <XmlAttribute> Public Property ints As Integer()
+    End Class
+
+    Public Class uints
+        <XmlAttribute> Public Property uints As UInteger()
+    End Class
+
+    Public Class longs
+        <XmlAttribute> Public Property longs As Long()
+    End Class
+
+    Public Class ulongs
+        <XmlAttribute> Public Property ulongs As ULong()
+    End Class
+End Namespace
